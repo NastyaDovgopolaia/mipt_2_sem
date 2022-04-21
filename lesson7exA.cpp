4ca6a3036d316b972036b9f37355ac367623e8fe
NastyaDovgopolaia <91183611+NastyaDovgopolaia@users.noreply.github.com>
2022-04-21 16:33:06 +0300

    Add files via upload

---
+++ lesson7exA.cpp
@@ -0,0 +1,48 @@
+# include <iostream>
+
+using namespace std;
+
+struct person {
+    char name[20];
+    int mark;
+};
+
+int main() {
+
+    int middle;
+    char arr_student[5][20];
+    int arr_mark[5];
+    for (int i = 0; i < 5; i++) {
+        person student;
+        cin >> student.name >> student.mark;
+        for (int j = 0; j < 20; j++) {
+            arr_student[i][j] = student.name[j];
+        }
+        arr_mark[i] = student.mark;
+    }
+
+    cin >> middle;
+
+    for (int i = 0; i < 5; i++) {
+        cout << arr_mark[i] << "\t";
+    }
+    cout << endl;
+
+    for (int i = 0; i < 5; i++) {
+        cout << arr_student[i] << "\t";
+    }
+    cout << endl;
+
+    for (int i = 0; i < 5; i++) {
+        if (arr_mark[i] < middle) {
+            arr_mark[i] = 0;
+        }
+
+        if (arr_mark[i] != 0) {
+            cout << arr_student[i] << "\t";
+        }
+    }
+
+    cout << endl;
+
+}
